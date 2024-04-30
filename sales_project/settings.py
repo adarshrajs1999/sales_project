8ace4f8d85e2e63d0ad5715544d3737871705e80
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-04-30 22:16:01 +0530

    today

--- sales_project/settings.py
+++ sales_project/settings.py
@@ -132,6 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL=('sales_app.Login')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
