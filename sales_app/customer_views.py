f386c3d41e73f63e8b681710dd189014570f894b
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-05-05 22:19:50 +0530

    today

--- sales_app/customer_views.py
+++ sales_app/customer_views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from sales_app import filters
 from sales_app.models import mobileproduct
 from sales_app.filters import  product_filter_form
 
 def customer_view_products(request):
     data = mobileproduct.objects.all()
     searched_form = product_filter_form(request.GET,queryset=data)
+    # qs-->query set
     data =searched_form.qs
     context={'data':data,'searched_form':searched_form}
     return render(request,"customer/view_products.html",context)
