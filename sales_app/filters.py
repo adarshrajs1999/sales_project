37bd345ad977bababd4702f0734e7888c748cede
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-05-07 22:31:15 +0530

    today

--- sales_app/filters.py
+++ sales_app/filters.py
@@ -8,9 +8,9 @@ from .models import mobileproduct, Seller
 # creating a dynamic filter form
 class product_filter_form(FilterSet):
     brand=CharFilter(lookup_expr='icontains',widget=forms.TextInput(
-        attrs={'class':'form-control','placeholder':'search brand'}))
+        attrs={'class':'form-control','placeholder':'search by brand'}))
     seller_name= CharFilter(field_name='seller__name',lookup_expr='icontains', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'search seller name'}))
+        attrs={'class': 'form-control', 'placeholder': 'search by seller name'}))
 
     class Meta:
         model = mobileproduct
